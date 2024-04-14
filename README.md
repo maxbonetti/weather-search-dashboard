d07b09c564c7cfbb1adfd854530618b79f56eff7
Max Bonetti <maxwell.bonetti@gmail.com>
2024-04-13 19:57:48 -0500

    most mark up done. added sopme function for API calls.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # weather-search-dashboard
 A weather application for when a user travels to a city they are able to see the days forecast as well as the 5 day forecast. leaving last searched citys to local storage for fast "click" search
+
+## description - 
+In this Weather Dashboard repo I was tasked with creating an application dashboard from scratch through the OpenWeather API. Doing this and holding values with localstorage to see old city names searched, as well as allowing 2 API calls at once. Seeing the current forecast for the city searched and also seeing a weekly forecast for that city below.
+
+## visual - 
+![The Weather Dashboard App webpage full screenshot layout view]()
+
+
+## links - 
+
+https://github.com/maxbonetti?tab=repositories
+
+## technolgy -
+HTML 5,
+CSS 3,
+JavaScript,
+Bootstrap CSS,
+Google Fonts,
+Awesome Fonts,
+OpenWeather API
+
+
+
+## credits - 
+edX UT Bootcamp
+ Friends in tech. - Sam Argentari, Cayden Humphries
+
+
+## license -
+refer to license in repo.
+MIT
+BootStrap CSS
+OpenWeather API
---
+++ assets/develop/javascript/script.js
@@ -0,0 +1,87 @@
+//get global variables from markup
+//create events for city search
+//allow user to see current weather forecast
+// below display 5day weather forecast
+//allow old cities searched below search bar for better user experience
+//local storage for old cities searched
+// local storage for forecasts 
+//make sure display emoji based on weather
+//EXTRA EXTRA EXTRA
+// create light/dark mode button again
+// old cities from local storage appear in new tab instead of take over main screen
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~GLOBAL VARIABLES~~~~~//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+const weatherFormEl = document.getElementById(`weahter-form`);
+const oldSearchBtns = document.getElementById(`city-buttons`);
+const cityInput = document.getElementById(`city-name`);
+const cityDayContEl = document.getElementById(`city-day-container`);
+const cityWeekContEl = document.getElementById(`city-week-container`);
+const citySearchTerm = document.getElementById(`city-search-term`);
+const apiUrlCurrent = `api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=0b6d015b759c89351c44e08524c52f32`;
+const apiUrlWeek = `api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=0b6d015b759c89351c44e08524c52f32`;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~FUNCTIONS~~~~~//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//function to reset the search bar and forms
+// then we want to get city forecast by fetching from the API
+// for both current and week
+//get current forecast
+// then get week forecast
+//when we get and save to local storage
+//then display current and weekly forecast
+// when we display create new HTML elements
+//then add event listeners and invoke functions
+const formSubmitHandler = function (event) {
+    event.preventDefault();
+    const cityName = cityInput.ariaValueMax.trim();
+    if (cityName) {
+        getCityForecast(cityName);
+
+        cityDayContEl.textContent = ``;
+        cityWeekContEl.textContent= ``;
+        cityInput.textContent= ``;
+    } else {
+        alert(`You must enter a city name.`)
+    }
+};
+//GET current weather function from API
+const getCurrentWeather = function (city) {
+    fetch(apiUrlCurrent)
+        .then(function (responseCurrent) {
+            if(responseCurrent.ok) {
+                console.log(responseCurrent);
+                responseCurrent.json().then(function (dataCurrent) {
+                    console.log(dataCurrent);
+                    displayCurrentWeather(dataCurrent, city);
+                });
+            }else {
+                alert(`Error: ${responseCurrent.statusText}`);
+            }
+        })
+        .catch(function (error) {
+            alert(`Unable to connect to OpenWeatherAPI`)
+        });
+};
+//GET week of weather forecast function from API
+const getWeeklyWeather = function (cityWeekly) {
+    fetch(apiUrlWeek)
+        .then(function (responseWeekly) {
+            if(responseWeekly.ok) {
+                console.log(responseWeekly);
+                responseWeekly.json().then(function (dataWeekly) {
+                    console.log(dataWeekly);
+                    displayWeeklyWeather(dataWeekly, city);
+                });
+            }else {
+                alert(`Error: ${responseWeekly.statusText}`);
+            }
+        })
+        .catch(function (errorWeekly) {
+            alert(`Unabale to connect to OpenWeatherAPI`)
+        });
+};
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+//~~~~~INVOKES~~~~~//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//  
